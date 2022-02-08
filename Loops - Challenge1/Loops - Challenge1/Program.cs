91aef13b391accad676d669ecc06d821ddee9c0f
webtracer <74070641+webtracer@users.noreply.github.com>
2022-02-08 09:13:13 -0800

    Add files via upload

---
+++ Loops - Challenge1/Loops - Challenge1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Loops___Challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int finished = 0;
+            double numScores = 0;
+            double avgScore;
+            double scores = 0;
+            int temp_score;
+            string userContinue;
+
+            while (finished != -1)
+            {
+                string userInput;
+
+                Console.WriteLine("Please enter the students score: ");
+                userInput = Console.ReadLine();
+                
+                try
+                {
+                    temp_score = int.Parse(userInput);
+                    if(temp_score > 20 || temp_score < -1)
+                    {
+                        Console.WriteLine("You did not enter a valid test score");
+                    }
+                    else
+                    {
+                        scores += temp_score;
+                    }
+                    // if(int.TryParse(input, out currentNumber) && currentNumber > 0 && currentNumber < 21)
+                    // that is a valid if statement with multiple conditions in it
+                }
+                catch(FormatException)
+                {
+                    //throw;
+                    Console.WriteLine("You have entered an invalid score, please try again");
+                }
+
+                Console.WriteLine("Would you like to enter another score?  Y for yes, -1 for no");
+                userContinue = Console.ReadLine();
+
+                if (userContinue == "-1")
+                {
+                    finished = -1;
+                }
+                else
+                {
+                    numScores++;
+                }
+            }
+
+            avgScore = scores / numScores;
+
+            Console.WriteLine($"The average score for {numScores} students is {avgScore}");
+        }
+    }
+}
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // For Loop - Great for counters
+            // for(start value; condition; increment)
+            // {
+            //      code body
+            // }
+
+            for(int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+            }
+
+            for(int counter = 0; counter < 50; counter += 5)
+            {
+                Console.WriteLine(counter);
+            }
+
+            //small challenge - for loop that only prints odd numbers
+            for(int counter = 0; counter < 20; counter++)
+            {
+                if(counter % 2 != 0)
+                {
+                    Console.WriteLine(counter);
+                }
+            }
+
+            // OR
+            for(int counter = 1; counter < 20; counter += 2)
+            {
+                Console.WriteLine(counter);
+            }
+
+            // While Loop - executes code while conditon is met
+            // counterVariable = 0;
+            //while(condition)
+            //{
+            //      execute your code
+            //      counterVariable++;
+            //}
+
+            int counterWhile = 0;
+
+            while(counterWhile < 10)
+            {
+                Console.WriteLine($"The While counter is {counterWhile}");
+                counterWhile++;
+            }
+
+            int peopleCounter = 0;
+            string allAboard = "";
+
+            while (allAboard  == "")
+            {
+                Console.Write("Please press enter to count people:");
+                allAboard = Console.ReadLine();
+                if (allAboard == "")
+                {
+                    Console.WriteLine($"The number of people is currently {peopleCounter}");
+                    peopleCounter++;
+                }
+                else
+                {
+                    Console.WriteLine("Everyone is on board, have a great trip!");
+                }
+            }
+
+
+            // Do While Loop - executes as long as the condition is met
+            // counterVariable = 0;
+            // do
+            // {
+            //       execute your code 
+            //      counterVariable++;
+            // }while(condition)   - will run at least once before condition is checked
+
+            int counterDo = 0;
+
+            do
+            {
+                Console.WriteLine($"The Do While counter is {counterDo}");
+                counterDo++;
+            } while (counterDo < 10);
+
+            int lengthOfText = 0;
+            string wholeText = "";
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend:");
+                string nameOfFriend = Console.ReadLine();
+                int currentLength = nameOfFriend.Length;
+                lengthOfText += currentLength;
+                wholeText += nameOfFriend + ", ";
+            } while (lengthOfText < 20);
+            Console.WriteLine("THat is enough names for now, thank you!");
+            Console.WriteLine($"Your friends: {wholeText}");
+
+            // break and continue
+
+            for(int counterBreak = 0; counterBreak < 10; counterBreak++)
+            {
+                Console.WriteLine(counterBreak);
+                if(counterBreak == 3)
+                {
+                    Console.WriteLine("The code 'breaks' at 3");
+                    break;
+                }
+            }
+            Console.WriteLine("This is outside of the for loop and the last number should be 3");
+
+            for(int counterContinue = 0; counterContinue < 10; counterContinue++)
+            {
+                if(counterContinue % 2 == 0)
+                {
+                    Console.WriteLine("THe next number is odd");
+                    continue;
+                }
+                Console.WriteLine(counterContinue);
+            }
+
+            //  foreach Loop
+            //  runs through an array
+            //  only runs as long as there is content
+            // Explained in the Arrays chapter
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
